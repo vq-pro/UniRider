e1ab664bc9ca8f8f41679c260110320bfddd7064
Patrick Robert <pro@virtualite.quebec>
2021-02-18 22:38:08 -0500

    Calculator mode - Different wheels

--- app/src/main/kotlin/quebec/virtualite/unirider/services/CalculatorService.kt
+++ app/src/main/kotlin/quebec/virtualite/unirider/services/CalculatorService.kt
@@ -9,7 +9,16 @@ class CalculatorService {
     fun batteryOn(text: String, highestF: Float, lowestF: Float): String {
 
         if (!isEmpty(text)) {
-            val voltage = text.replace(".", "").toInt()
+
+            val voltage = when {
+                text.indexOf('.') == -1 ->
+                    text.toInt() * 10
+                text.endsWith('.') ->
+                    text.replace(".", "0").toInt()
+                else ->
+                    text.replace(".", "").toInt()
+            }
+
             val highest = (highestF * 10).roundToInt()
             val lowest = (lowestF * 10).roundToInt()
             val percentage: Float = (voltage - lowest) * 100f / (highest - lowest)
--- app/src/test/kotlin/quebec/virtualite/unirider/services/CalculatorServiceTest.kt
+++ app/src/test/kotlin/quebec/virtualite/unirider/services/CalculatorServiceTest.kt
@@ -15,16 +15,28 @@ class CalculatorServiceTest {
 
     @Test
     fun batteryOn() {
+        // Gotway Nikola
+        batteryOn("100.8", 100.8f, 79.2f, "100%")
+        batteryOn("96.4", 100.8f, 79.2f, "79.6%")
+        batteryOn("95.0", 100.8f, 79.2f, "73.1%")
+        batteryOn("95.", 100.8f, 79.2f, "73.1%")
+        batteryOn("95", 100.8f, 79.2f, "73.1%")
+        batteryOn("89.1", 100.8f, 79.2f, "45.8%")
+        batteryOn("79.2", 100.8f, 79.2f, "0%")
+
+        // Inmotion V10F
+        batteryOn("74.6", 84f, 68f, "41.3%")
+
+        // KingSong 14D
+        batteryOn("63.5", 67.2f, 48.0f, "80.7%")
+
+        // Veteran Sherman
+        batteryOn("96.5", 100.8f, 75.6f, "82.9%")
+
+        // Invalid values
         batteryOn("", 100.8f, 79.2f, "")
         batteryOn("9", 100.8f, 79.2f, "")
         batteryOn("63.5", 0f, 0f, "")
-        batteryOn("96.4", 100.8f, 79.2f, "79.6%")
-        batteryOn("89.1", 100.8f, 79.2f, "45.8%")
-        batteryOn("100.8", 100.8f, 79.2f, "100%")
-        batteryOn("79.2", 100.8f, 79.2f, "0%")
-        batteryOn("63.5", 67.2f, 48.0f, "80.7%")
-        batteryOn("96.5", 100.8f, 75.6f, "82.9%")
-        batteryOn("74.6", 84f, 68f, "41.3%")
     }
 
     internal fun batteryOn(voltage: String, highest: Float, lowest: Float, expectedBattery: String) {
