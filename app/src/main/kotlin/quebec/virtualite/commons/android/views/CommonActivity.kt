0a85e825707d0aaa72a6764fa062e2ea293f2c05
Patrick Robert <pro@virtualite.quebec>
2026-09-21 12:30:25 -0400

    Height

--- app/src/main/kotlin/quebec/virtualite/commons/android/views/CommonActivity.kt
+++ app/src/main/kotlin/quebec/virtualite/commons/android/views/CommonActivity.kt
@@ -8,6 +8,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat.requestPermissions
 import androidx.core.app.ActivityCompat.shouldShowRequestPermissionRationale
 import androidx.core.content.ContextCompat.checkSelfPermission
+import androidx.core.view.WindowCompat
 
 open class CommonActivity(
     private val idMainLayout: Int,
@@ -16,37 +17,48 @@ open class CommonActivity(
     private val idActionSettings: Int,
     private vararg val requiredPermissions: String
 
-) : AppCompatActivity() {
-
-    public override fun onCreate(savedInstanceState: Bundle?) {
+) : AppCompatActivity()
+{
+    public override fun onCreate(savedInstanceState: Bundle?)
+    {
         super.onCreate(savedInstanceState)
         setContentView(idMainLayout)
         setSupportActionBar(findViewById(idToolbar))
 
-        for (permission in requiredPermissions) {
+        for (permission in requiredPermissions)
+        {
             requestPermissionIfNotGranted(permission)
         }
+
+        // Force status bar icons (time, battery, etc.) to be dark/visible
+        WindowCompat.getInsetsController(window, window.decorView).isAppearanceLightStatusBars = true
     }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+    override fun onCreateOptionsMenu(menu: Menu): Boolean
+    {
         // Inflate the menu; this adds items to the action bar if it is present.
         menuInflater.inflate(idMenu, menu)
         return true
     }
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+    override fun onOptionsItemSelected(item: MenuItem): Boolean
+    {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
+        return when (item.itemId)
+        {
             idActionSettings -> true
             else -> super.onOptionsItemSelected(item)
         }
     }
 
-    private fun requestPermissionIfNotGranted(permission: String) {
-        if (checkSelfPermission(this, permission) != PERMISSION_GRANTED) {
-            if (shouldShowRequestPermissionRationale(this, permission)) {
+    private fun requestPermissionIfNotGranted(permission: String)
+    {
+        if (checkSelfPermission(this, permission) != PERMISSION_GRANTED)
+        {
+            if (shouldShowRequestPermissionRationale(this, permission))
+            {
                 requestPermissions(this, arrayOf(permission), 1)
             }
         }
